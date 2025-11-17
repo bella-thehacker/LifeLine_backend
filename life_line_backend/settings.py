d5ec727492f1f64d28ed38286d9845bc815e4e54
Ivy Mburu <ikagam018@gmail.com>
2025-11-17 03:54:58 -0800

    finished

--- life_line_backend/settings.py
+++ life_line_backend/settings.py
@@ -58,6 +58,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5174",
 ]
 
 ROOT_URLCONF = 'life_line_backend.urls'
